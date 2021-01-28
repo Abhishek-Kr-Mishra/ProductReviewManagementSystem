4ff084a37ce11f50c6998c1254ad228add784ba1
abh224272 <abh224272@gmail.com>
2021-01-28 16:45:24 +0530

    [Abhishek Mishra] Add: Created Method to add default values in DataTable

--- ProductReviewManagement/Management.cs
+++ ProductReviewManagement/Management.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 
@@ -7,6 +8,7 @@ namespace ProductReviewManagement
 {
     public class Management
     {
+        public readonly DataTable dataTable = new DataTable();
         public void TopRecords(List<ProductReview> review)
         {
 
@@ -56,6 +58,26 @@ namespace ProductReviewManagement
             {
                 Console.WriteLine("ProductID:-" + list.ProductID + " " + "UserID:-" + list.UserID
                     + " " + "Rating:-" + list.Rating + " " + "Review:-" + list.Review + " " + "isLike:-" + list.isLike);
+            }
+        }
+        public void ProductReviewsDataTable(List<ProductReview> review)
+        {
+            var recordedData = from productReviews in review
+                               select productReviews;
+            dataTable.Columns.Add("ProductId").DataType = typeof(Int32);
+            dataTable.Columns.Add("UserId").DataType = typeof(Int32);
+            dataTable.Columns.Add("Rating").DataType = typeof(double);
+            dataTable.Columns.Add("Review");
+            dataTable.Columns.Add("isLike").DataType = typeof(bool);
+            foreach (var list in recordedData)
+            {
+                dataTable.Rows.Add(list.ProductID, list.UserID, list.Rating, list.Review, list.isLike);
+            }
+            var productTable = from products in dataTable.AsEnumerable() select products;
+            foreach (DataRow product in productTable)
+            {
+                Console.WriteLine(product.Field<int>("productId") + " " + product.Field<int>("UserId") + " "
+                    + product.Field<double>("Rating") + " " + product.Field<string>("Review") + " " + product.Field<bool>("isLike"));
             }
         }
     }
